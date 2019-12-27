aac6e0f587e674f98a8a1c48a75a65b72dee7b69
jeffrey6052 <jeffrey6052@163com>
2019-12-27 20:16:17 +0800

    电容器组 svg

--- src/pages/ElectricalSymbol/index.js
+++ src/pages/ElectricalSymbol/index.js
@@ -5,7 +5,11 @@ import { Button } from 'antd'
 import Layout from '../../components/Layout'
 
 import curveLineIcons from '../../assets/svg/jowo0115_icons/curve_line.svg'
-import switchIcons from '../../assets/svg/jowo0115_icons/switch.svg'
+import switch01Icons from '../../assets/svg/jowo0115_icons/switch01.svg'
+import switch02Icons from '../../assets/svg/jowo0115_icons/switch02.svg'
+import groundIcons from '../../assets/svg/jowo0115_icons/ground.svg'
+import currentTransformerIcons from '../../assets/svg/jowo0115_icons/current_transformer.svg'
+import capacitorBankIcons from '../../assets/svg/jowo0115_icons/capacitor_bank.svg'
 
 import exampleIcons from '../../assets/svg/example_icons.svg'
 import electricalSymbols from '../../assets/svg/electrical_symbols.svg'
@@ -39,8 +43,8 @@ class ElectricalSymbol extends React.Component {
     render_dis1() {
         const x = 0
         const y = 50
-        const width = 81
-        const height = 81
+        const width = 96
+        const height = 96
         const stroke = "black"
         const strokeOpacity = "1"
         const symbolName = this.state.dis1Open ? "Dis_V_D_Open" : "Dis_V_D_Close"
@@ -58,78 +62,286 @@ class ElectricalSymbol extends React.Component {
         )
     }
 
+    render_switch01() {
+        return (
+            <>
+                <div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_V_D_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_V_U_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_H_L_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_H_R_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                </div>
+                <div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_V_D_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_V_U_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_H_L_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch01Icons}#Switch01_H_R_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    render_switch02() {
+        return (
+            <>
+                <div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_V_D_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_V_U_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_H_L_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_H_R_Open`} stroke="black" />
+                        </svg>
+                    </div>
+                </div>
+                <div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_V_D_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_V_U_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_H_L_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${switch02Icons}#Switch02_H_R_Close`} stroke="black" />
+                        </svg>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    render_curve() {
+        return (
+            <div>
+                <div style={{ margin: 20, display: "inline-block" }}>
+                    <svg
+                        style={{ width: 96, height: 96, background: "#eee" }}
+                        viewBox="0 0 96 96"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                    >
+                        <use
+                            xlinkHref={`${curveLineIcons}#CurveLine-L`}
+                            stroke="black"
+                            strokeWidth={1}
+                        />
+                    </svg>
+                </div>
+
+                <div style={{ margin: 20, display: "inline-block" }}>
+                    <svg
+                        style={{ width: 96, height: 96, background: "#eee" }}
+                        viewBox="0 0 96 96"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                        strokeWidth={1}
+                    >
+                        <use xlinkHref={`${curveLineIcons}#CurveLine-R`} stroke="black" />
+                    </svg>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
                 <h1>电路图元件</h1>
 
                 <h3>弯线</h3>
+                {this.render_curve()}
+                <hr />
 
-                <div>
-                    <div style={{ margin: 20, display: "inline-block" }}>
-                        <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                            preserveAspectRatio="none"
-                        >
-                            <use
-                                xlinkHref={`${curveLineIcons}#CurveLine-L`}
-                                stroke="black"
-                                strokeWidth={81 / 81}
-                            />
-                        </svg>
-                    </div>
-
-                    <div style={{ margin: 20, display: "inline-block" }}>
-                        <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                            strokeWidth={81 / 81}
-                        >
-                            <use xlinkHref={`${curveLineIcons}#CurveLine-R`} stroke="black" />
-                        </svg>
-                    </div>
-                </div>
-
+                <h3>隔断开关</h3>
+                {this.render_switch01()}
                 <hr />
 
                 <h3>抽屉式断路器</h3>
+                {this.render_switch02()}
+                <hr />
+
+                <h3>接地</h3>
                 <div>
                     <div style={{ margin: 20, display: "inline-block" }}>
                         <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
                         >
-                            <use xlinkHref={`${switchIcons}#Switch_V_D_Open`} stroke="black" />
-                        </svg>
-                    </div>
-
-                    <div style={{ margin: 20, display: "inline-block" }}>
-                        <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                        >
-                            <use xlinkHref={`${switchIcons}#Switch_V_D_Close`} stroke="black" />
+                            <use xlinkHref={`${groundIcons}#Ground`} stroke="black" />
                         </svg>
                     </div>
                 </div>
 
-                <hr />
+                <h3>电流互感器</h3>
+                <div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${currentTransformerIcons}#CurrentTransformer`} stroke="black" />
+                        </svg>
+                    </div>
+                </div>
+
+                <h3>电容器组</h3>
+                <div>
+                    <div style={{ margin: 20, display: "inline-block" }}>
+                        <svg
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref={`${capacitorBankIcons}#CapacitorBank`} stroke="black" />
+                        </svg>
+                    </div>
+                </div>
 
                 <h3>测试</h3>
                 <div>
                     <div style={{ margin: 20, display: "inline-block" }}>
                         <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
                         >
@@ -139,8 +351,8 @@ class ElectricalSymbol extends React.Component {
 
                     <div style={{ margin: 20, display: "inline-block" }}>
                         <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
                         >
@@ -150,8 +362,8 @@ class ElectricalSymbol extends React.Component {
 
                     <div style={{ margin: 20, display: "inline-block" }}>
                         <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
                         >
@@ -161,15 +373,15 @@ class ElectricalSymbol extends React.Component {
 
                     <div style={{ margin: 20, display: "inline-block" }}>
                         <svg
-                            style={{ width: 81, height: 81 }}
-                            viewBox="0 0 81 81"
+                            style={{ width: 96, height: 96, background: "#eee" }}
+                            viewBox="0 0 96 96"
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
                         >
                             <use xlinkHref={`${exampleIcons}#DrawoutDis_V_Close`} stroke="black" />
                         </svg>
                     </div>
-        
+
                 </div>
 
                 <hr />
